43ca11f2da5a3fffc672b6f7917b31f230dccee7
Dodo-1989 <naderzaeri89@gmail.com>
2020-01-25 15:03:48 -0600

    This is a minor correction

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
        console.log(`Hey, ${cookName}! This is my marinara sauce recpie!`);
        console.log(" - Heat a medium-large saucepan over medium heat.")
        console.log("- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes".replace(two, 'two'))
-       console.log(" - Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.".replace(oneFourth, 'oneFourth').replace(two, 'oneFourth'))
+       console.log(" - Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.".replace(oneFourth, 'oneFourth').replace(two, 'two'))
        console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes")
        console.log("- Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.".replace(oneFourth, 'onefourth'))
 }
